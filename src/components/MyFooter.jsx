01a5231e3d120b868b6af962855d9f5d42a8bd4e
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-07-23 13:42:07 +0500

    Footer added

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+
 export default function MyFooter() {
     return (
-        <footer className="fixed bottom-0 w-full">
-            <h1>this is a footer</h1>
+        <footer className="fixed bottom-0 w-full , bg-[#CD2C58]" >
+            <p className="text-[#FFE6D4] ,  text-center ">&copy; 2026 This is the project to showcase my frontend skills.</p>
+            <h1 className="text-center text-[#FFE6D4]">About</h1>
         </footer>
     );
 }
